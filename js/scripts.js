0febedb5a8b182f4b1993de112f5f1a35ff439e4
Peter Jurkovic <petojurkovic@gmail.com>
2014-03-25 00:37:39 +0100

    Skills animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,18 @@
 $(function() {
-	var $items = $('#skill-wrapp div'),
-	positionList = elements($items);
-	
-	//$items.css({ top: '-2500', left: '0' })
+	animateSkills();
 });
 
 
-function elements($items){
-	var arr = [],
-		wh = $(document).outerWidth();
-
-	$items.each(function(i){
-		var $this = $(this),
-			offset = $this.offset();
-		console.log(offset);
-		$this.transition({ x: (i % 2 === 0 ? ((wh * -1) - 450) : wh + 450) })
-		  .show()
-		  .transition({ y: 200 })
-		  .transition({ x: 0 })
-		  .transition({ y: 0 });	
-		console.log($this.offset());
-		arr.push($this.position());
-		console.log('---');
-	});
-	return arr;
+function animateSkills(){
+	var windowWidth = $(document).outerWidth(),
+		$items = $('#skill-wrapp div');
+		if(windowWidth >= 920){
+			$items.each(function(i){
+				var $this = $(this);
+				$this.transition({ x: (i % 2 === 0 ? ((windowWidth * -1) - 450) : windowWidth + 450), delay: i * 15 })
+				  .transition({ y: 200 })
+				  .transition({ x: 0 })
+				  .transition({ y: 0 });	
+			});
+		}
 }
