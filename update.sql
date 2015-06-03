6a3ce6c6d8eda922073f459e1d6c4220a1a8e846
peterjurkovic <petojurkovic@gmail.com>
2015-06-03 22:57:03 +0100

    Bower,Angular,Bootstrap,Grunt skills were added

---
+++ update.sql
@@ -0,0 +1,2 @@
+INSERT INTO `portfolio_peterjurkovic_sk`.`skill` (`id`, `name`, `css_class`, `code`, `descr_sk`, `descr_en`, `enabled`) VALUES (NULL, 'AngularJS', NULL, '', NULL, NULL, '1'), (NULL, 'Bootstrap', NULL, '', NULL, NULL, '1');
+INSERT INTO `portfolio_peterjurkovic_sk`.`skill` (`id`, `name`, `css_class`, `code`, `descr_sk`, `descr_en`, `enabled`) VALUES (NULL, 'Bower', NULL, '', NULL, NULL, '1'), (NULL, 'Grunt', NULL, '', NULL, NULL, '1');
